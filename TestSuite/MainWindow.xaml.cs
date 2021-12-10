cb53f6445640374444d686090f42bd4e46c0c03f
Aiden Meikle <aidenmeikle@ymail.com>
2021-12-10 02:04:47 +0000

    Closes #3 Render Skeleton Hands
    For each of the 6 possible bodies get the location of the Left & Right
    Hands
    Converts the Camera Space Coordinates to Depth Space
    Set the Hands to be Ellipse shapes

--- TestSuite/MainWindow.xaml.cs
+++ TestSuite/MainWindow.xaml.cs
@@ -23,14 +23,130 @@ namespace TestSuite
     public partial class MainWindow : Window
     {
 
-        KinectSensor kinectSensor;
+        // Represents the Kinect Device, used to gather data on Persons in View
+        private KinectSensor kinectSensor;
+        private MultiSourceFrameReader multiSourceFrameReader;
+
+        private FrameSourceTypes enabledFramesSources =
+            FrameSourceTypes.Body
+            | FrameSourceTypes.Color
+            | FrameSourceTypes.Depth
+            | FrameSourceTypes.Infrared
+            | FrameSourceTypes.BodyIndex;
+
+        private IDictionary<int, List<Ellipse>> bodyHands;
 
         public MainWindow()
         {
             kinectSensor = KinectSensor.GetDefault();
+
+            multiSourceFrameReader = kinectSensor.OpenMultiSourceFrameReader(enabledFramesSources);
+            multiSourceFrameReader.MultiSourceFrameArrived += MainWindow_MultiSourceFrameArrived;
+
+            Loaded += MainWindow_Loaded;
+
+            bodyHands = new Dictionary<int, List<Ellipse>>();
+            
+
             kinectSensor.Open();
 
             InitializeComponent();
         }
+
+        #region WindowLifecycleEvents
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < kinectSensor.BodyFrameSource.BodyCount; i++)
+            {
+                Ellipse leftHand = new Ellipse()
+                {
+                    Fill = Brushes.Yellow,
+                    Height = 30,
+                    Width = 30,
+                    Visibility = Visibility.Collapsed
+                };
+                Ellipse rightHand = new Ellipse()
+                {
+                    Fill = Brushes.Yellow,
+                    Height = 30,
+                    Width = 30,
+                    Visibility = Visibility.Collapsed
+                };
+
+                List<Ellipse> hands = new List<Ellipse>();
+                hands.Add(leftHand);
+                hands.Add(rightHand);
+
+                bodyHands.Add(i, hands);
+                SkeletonGrid.Children.Add(leftHand);
+                SkeletonGrid.Children.Add(rightHand);
+            }
+        }
+        #endregion
+
+        #region KinectEventCallback
+        private void MainWindow_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
+
+            if (multiSourceFrame == null)
+            {
+                return;
+            }
+
+            using (BodyFrame bodyFrame = multiSourceFrame.BodyFrameReference.AcquireFrame())
+            {
+                Body[] bodies = new Body[kinectSensor.BodyFrameSource.BodyCount];
+
+                bodyFrame.GetAndRefreshBodyData(bodies);
+
+                for (int i = 0; i < bodies.Length; i++)
+                {
+                    Body body = bodies[i];
+                    Joint leftHand = body.Joints[JointType.HandLeft];
+                    Joint rightHand = body.Joints[JointType.HandRight];
+
+                    if (leftHand.Position.Z < 0)
+                    {
+                        leftHand.Position.Z = .1f;
+                    }
+
+                    if (rightHand.Position.Z < 0)
+                    {
+                        rightHand.Position.Z = .1f;
+                    }
+
+                    Ellipse leftHandEllipse = bodyHands[i][0];
+                    Ellipse rightHandEllipse = bodyHands[i][1];
+
+                    CameraSpacePoint[] handCameraPoints = { leftHand.Position, rightHand.Position };
+                    DepthSpacePoint[] handDepthPoints = new DepthSpacePoint[2];
+
+                    kinectSensor.CoordinateMapper.MapCameraPointsToDepthSpace(handCameraPoints, handDepthPoints);
+
+                    leftHandEllipse.Visibility = leftHand.TrackingState == TrackingState.NotTracked ?
+                                                        Visibility.Collapsed : Visibility.Visible;
+
+                    rightHandEllipse.Visibility = rightHand.TrackingState == TrackingState.NotTracked ?
+                                                        Visibility.Collapsed : Visibility.Visible;
+
+                    if (leftHand.TrackingState != TrackingState.NotTracked)
+                    {
+                        Point leftHandPoint = new Point(handDepthPoints[0].X, handDepthPoints[0].Y);
+                        Canvas.SetLeft(leftHandEllipse, leftHandPoint.X - leftHandEllipse.Width / 2);
+                        Canvas.SetTop(leftHandEllipse, leftHandPoint.Y - leftHandEllipse.Height / 2);
+
+
+                    }
+                    if (rightHand.TrackingState != TrackingState.NotTracked)
+                    {
+                        Point rightHandPoint = new Point(handDepthPoints[1].X, handDepthPoints[1].Y);
+                        Canvas.SetLeft(rightHandEllipse, rightHandPoint.X - rightHandEllipse.Width / 2);
+                        Canvas.SetTop(rightHandEllipse, rightHandPoint.Y - rightHandEllipse.Height / 2);
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
